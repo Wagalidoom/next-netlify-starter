c73deea53a8c5e053777dad501c4a1af3c4c5e46
Luka la d <lukas-8@live.fr>
2023-10-22 17:57:32 +0200

    add themed button

--- pages/link.js
+++ pages/link.js
@@ -7,15 +7,8 @@ export default function Link() {
     <div className="container">
       <main style={{ width: "100%", padding: 0 }}>
         <button
-          style={{
-            width: "100%",
-            height: "100%",
-            fontSize: "72px",
-            backgroundColor: "#1899D6",
-            border: "solid transparent",
-            borderRadius: "16px",
-            borderWidth: "0 0 4px",
-          }}
+          className="link"
+          style={{ width: "100%", height: "100%", fontSize: "72px" }}
           onClick={openTab}
         >
           Click me
