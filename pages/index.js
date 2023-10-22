e35bfa77ac9bdcd3f7091e25751f88170bd0a770
Luka la d <lukas-8@live.fr>
2023-10-22 17:19:54 +0200

    add iframe on cyber

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,24 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
 
 export default function Home() {
-  const openTab= () => {
-    window.open('https://www.agence-morse.fr/','_blank')
+  const openTab = () => {
+    window.open("https://www.agence-morse.fr/", "_blank");
   };
 
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Morse</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main style={{width: "100%", padding: 0}}>
-        <button style={{width: "100%", height: "100%", fontSize: "72px"}} onClick={openTab}>Click me</button>
+      <main style={{ width: "100%", padding: 0 }}>
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://oncyber.io/spaces/vR8qMW42qsj0VtQCqe4r?coords=-12.13x2.82x-17.51x-3.04"
+        ></iframe>
       </main>
     </div>
-  )
+  );
 }
---
+++ pages/redirect.js
@@ -0,0 +1,13 @@
+export default function Redirect() {
+  const openTab= () => {
+    window.open('https://www.agence-morse.fr/','_blank')
+  };
+
+  return (
+    <div className="container">
+      <main style={{width: "100%", padding: 0}}>
+        <button style={{width: "100%", height: "100%", fontSize: "72px"}} onClick={openTab}>Click me</button>
+      </main>
+    </div>
+  )
+}
