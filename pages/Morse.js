8b874a2d2dc68ca5e45cb36991c5b3e5c7ac8328
Luka la d <lukas-8@live.fr>
2023-10-24 17:10:46 +0200

    add clickable img website

--- pages/Morse.js
+++ pages/Morse.js
@@ -1,3 +1,5 @@
+import morse from "../public/morse.png"
+
 export default function Morse() {
     const openTab= () => {
       window.open('https://www.agence-morse.fr/contacter-lagence-morse/','_blank')
@@ -6,7 +8,8 @@ export default function Morse() {
     return (
       <div className="container">
         <main style={{width: "100%", padding: 0}}>
-          <button style={{width: "100%", height: "100%", fontSize: "102px", fontWeight: "900", fontFamily: "Montserrat"}} onClick={openTab}>Par ici !</button>
+          <img style={{width: "100%", height: "100%"}} src={morse} onClick={openTab} />
+          {/* <button style={{width: "100%", height: "100%", fontSize: "102px", fontWeight: "900", fontFamily: "Montserrat"}} onClick={openTab}>Par ici !</button> */}
         </main>
       </div>
     )
