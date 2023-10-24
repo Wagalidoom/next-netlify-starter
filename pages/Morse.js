8fbd98cd9f5e1e422b92e3f4345df21ff669e346
Luka la d <lukas-8@live.fr>
2023-10-24 17:26:52 +0200

    update ing

--- pages/Morse.js
+++ pages/Morse.js
@@ -6,7 +6,7 @@ export default function Morse() {
     return (
       <div className="container">
         <main style={{width: "100%", padding: 0}}>
-          <img style={{width: "100%", height: "100%"}} src="/morse.png" onClick={openTab} />
+          <img style={{height: "100vh"}} src="/morse.png" onClick={openTab} />
           {/* <button style={{width: "100%", height: "100%", fontSize: "102px", fontWeight: "900", fontFamily: "Montserrat"}} onClick={openTab}>Par ici !</button> */}
         </main>
       </div>
