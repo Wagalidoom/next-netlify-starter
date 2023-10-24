4f00ee4f5a7391a360717ca2935afc804fa3a148
Luka la d <lukas-8@live.fr>
2023-10-24 17:24:14 +0200

    update img

--- pages/Morse.js
+++ pages/Morse.js
@@ -1,5 +1,3 @@
-import morse from "../public/morse.png"
-
 export default function Morse() {
     const openTab= () => {
       window.open('https://www.agence-morse.fr/contacter-lagence-morse/','_blank')
@@ -8,7 +6,7 @@ export default function Morse() {
     return (
       <div className="container">
         <main style={{width: "100%", padding: 0}}>
-          <img style={{width: "100%", height: "100%"}} src={morse} onClick={openTab} />
+          <img style={{width: "100%", height: "100%"}} src="/morse.png" onClick={openTab} />
           {/* <button style={{width: "100%", height: "100%", fontSize: "102px", fontWeight: "900", fontFamily: "Montserrat"}} onClick={openTab}>Par ici !</button> */}
         </main>
       </div>
